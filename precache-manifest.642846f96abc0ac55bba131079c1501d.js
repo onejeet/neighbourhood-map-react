f37cf8845c96f94667253abc83ffb19ffd7639e3
onejeet <jitendra909@gmail.com>
2018-10-29 23:28:11 +0530

    Updates

---
+++ precache-manifest.642846f96abc0ac55bba131079c1501d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ddfe0be87486762d2b8",
+    "url": "/react-neighbourhood-map/static/js/runtime~main.0ddfe0be.js"
+  },
+  {
+    "revision": "344b412ecae52f5850c7",
+    "url": "/react-neighbourhood-map/static/js/main.344b412e.chunk.js"
+  },
+  {
+    "revision": "aeaf817eed75b7b4479f",
+    "url": "/react-neighbourhood-map/static/js/1.aeaf817e.chunk.js"
+  },
+  {
+    "revision": "344b412ecae52f5850c7",
+    "url": "/react-neighbourhood-map/static/css/main.d0a2c660.chunk.css"
+  },
+  {
+    "revision": "054ba8230a5738ec5c1d537d94807d9a",
+    "url": "/react-neighbourhood-map/index.html"
+  }
+];
