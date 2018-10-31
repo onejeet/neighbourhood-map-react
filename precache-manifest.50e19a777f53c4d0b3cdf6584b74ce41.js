6a73a824c543272834a1444a331487a326605d34
onejeet <jitendra909@gmail.com>
2018-10-31 12:17:07 +0530

    Updates

---
+++ precache-manifest.50e19a777f53c4d0b3cdf6584b74ce41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ddfe0be87486762d2b8",
+    "url": "/react-neighbourhood-map/static/js/runtime~main.0ddfe0be.js"
+  },
+  {
+    "revision": "5d4cdc3600e08ee6a09e",
+    "url": "/react-neighbourhood-map/static/js/main.5d4cdc36.chunk.js"
+  },
+  {
+    "revision": "b30576282c7b1fe0ab21",
+    "url": "/react-neighbourhood-map/static/js/1.b3057628.chunk.js"
+  },
+  {
+    "revision": "5d4cdc3600e08ee6a09e",
+    "url": "/react-neighbourhood-map/static/css/main.135ceb5e.chunk.css"
+  },
+  {
+    "revision": "771f68f6ce65a3a3cf9b352f1d9ac2d6",
+    "url": "/react-neighbourhood-map/index.html"
+  }
+];
